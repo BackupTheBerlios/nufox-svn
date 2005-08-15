0a762cfc6713b14d813ebc7d64bc028eeb561941
cablehead <cablehead@7633b0e3-71fc-0310-99a7-da582406f301>
2005-08-15 09:18:53 +0000

    added a couple more examples

---
+++ trunk/examples/basicMenuList.py
@@ -0,0 +1,38 @@
+from nevow import livepage
+from nufox import xul
+
+class XULTKPage(xul.XULPage):
+
+    def __init__(self):
+        self.counter = 0
+        self.window = xul.Window(id="xul-window", height=400, width=400,
+                                 title="XUL is Cool")
+        v = xul.VBox(flex=1)
+
+        ml = xul.MenuList()
+        mp = xul.MenuPopup()
+        mp.append(xul.MenuItem(label='the value that be fred', value='fred'))
+        mp.append(xul.MenuItem(label='the value that be bork', value='bork'))
+        mp.append(xul.MenuItem(label='the value that be yerk', value='yerk'))
+        ml.append(mp)
+        ml.addHandler('oncommand', self.menuSelect)
+        self.menuList = ml
+        v.append(self.menuList)
+
+        self.label = xul.Label(value='hello there')
+        v.append(self.label)
+
+        self.window.append(v)
+
+    def menuSelect(self):
+        d = self.menuList.getAttr(self.client, 'value')
+        d.addBoth(log)
+        d.addCallback(lambda r, s, c:
+                s(c, 'value', 'you chose: %s' % r),
+            self.label.setAttr, self.client)
+
+def log(r):
+    print "LOGGING ",r
+    return r
+
+example = XULTKPage()
