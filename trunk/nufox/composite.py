e293f8685ca15ec021f99a1b69388832bce94544
tjs <tjs@7633b0e3-71fc-0310-99a7-da582406f301>
2005-09-10 05:45:33 +0000

    Added a grid box composite, useful for laying out forms

--- trunk/nufox/composite.py
+++ trunk/nufox/composite.py
@@ -5,6 +5,33 @@ from nufox import xul
 from nevow import livepage
 from twisted.internet.defer import Deferred
 
+class Grid(xul.GenericWidget):
+    """I take a list of lists of XUL widgets, which i will render in a grid.
+    The grid is length of list by length of the longest list, shorter lists
+    will be padded with xul.Spacer() widgets."""
+
+    def __init__(self, l):
+        xul.GenericWidget.__init__(self)
+        self.data = l
+        self.width = 0
+        for l in self.data:
+            if len(l) > self.width:
+                self.width = len(l)
+
+    def getTag(self):
+        grid = xul.Grid()
+        columns = xul.Columns()
+        rows = xul.Rows()
+        for i in range(self.width):
+            columns.append(xul.Column())
+        for l in self.data:
+            if len(l) < self.width:
+                for i in range(self.width - len(l)):
+                    l.append(xul.Spacer())
+            rows.append(xul.Row().append(*l))
+        grid.append(columns, rows)
+        return grid
+    
 class Player(xul.GenericWidget):
     """I am a media player, I require either the helix or realplayer plugin"""
 
