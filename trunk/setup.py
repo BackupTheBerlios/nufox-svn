41138d767a68f997d43525815ae561d425e99b54
mscott <mscott@7633b0e3-71fc-0310-99a7-da582406f301>
2005-11-03 14:55:29 +0000

    r5946@ra:  gldnspud | 2005-11-03 08:50:53 -0600
     r5947@ra:  gldnspud | 2005-11-03 08:54:36 -0600
     distutils/setuptools-based setup script
     r5948@ra:  gldnspud | 2005-11-03 08:55:16 -0600
     src dir, to keep nufox package separate from examples, etc. for distutils purposes

---
+++ trunk/setup.py
@@ -0,0 +1,48 @@
+"""Nufox setup."""
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+
+packages = find_packages('src')
+
+
+setup(
+    name = 'Nufox',
+    version = '0.2.0',
+    description = 'Python + XUL',
+    long_description = """
+    Nufox is a Python XUL_ toolkit written on top of Twisted_ and Nevow_.
+
+    .. _XUL: http://xulplanet.com/
+
+    .. _Twisted: http://twistedmatrix.com/
+
+    .. _Nevow: http://nevow.com/
+    """,
+    
+    classifiers = [
+    ## Fill me in.
+    ],
+    
+    author = 'Nufox Contributors',
+    author_email = 'nufox@googlegroups.com',
+    url = 'http://trac.nunatak.com.au/projects/nufox',
+    license = 'LGPL',
+    platforms = [
+    ## Fill me in.
+    ],
+
+    package_dir = {'': 'src'},
+    packages = packages,
+    package_data = {
+    ## Fill me in.
+    },
+
+    install_requires = [
+    ## Fill me in.
+    ],
+
+    )
