ebaf597a9c923eb3408bb9d7e2ec0ecb6b9bde27
gldnspud <gldnspud@7633b0e3-71fc-0310-99a7-da582406f301>
2005-12-22 15:17:39 +0000

    r40190@ra:  gldnspud | 2005-12-22 09:09:23 -0600  Extend example for MenuList

--- trunk/examples/nwMenulist.py
+++ trunk/examples/nwMenulist.py
@@ -5,6 +5,23 @@ from nufox.widget import signal, std
 from nufox import xul
 
 
+class CustomMenuItem(std.MenuItem):
+
+    def preInit(self, kwargs):
+        self._name = kwargs.pop('name')
+        return super(CustomMenuItem, self).preInit(kwargs)
+
+    def setup(self):
+        super(CustomMenuItem, self).setup()
+        self.set('value', self.id)
+        self._counter = 0
+
+    def counterMessage(self):
+        self._counter += 1
+        return u'You have selected "%s" %i times.' % (
+            self._name, self._counter)
+
+
 class Example(xul.XULPage):
     """nufox.widget: MenuList usage"""
 
@@ -27,6 +44,25 @@ class Example(xul.XULPage):
         # Create a label to show the value.
         label = self.label = vbox.adopt(
             std.Label(value=u'This label will contain the selected value.'))
+        # Spacer.
+        vbox.append(std.VBox(height=50))
+        # Custom menu item class usage.
+        menuList2 = vbox.adopt(std.MenuList())
+        menuPopup2 = menuList2.adopt(std.MenuPopup())
+        menuPopup2.append(
+            CustomMenuItem(label=u'My name is "fred"', name=u'fred'),
+            CustomMenuItem(label=u'My name is "bork"', name=u'bork'),
+            CustomMenuItem(label=u'My name is "yerk"', name=u'yerk'),
+            )
+        menuList2.connect(signal.itemSelected, self.on_menuList2_itemSelected)
+        label2 = self.label2 = vbox.adopt(
+            std.Label(value=u'This label will contain a counter.'))
 
     def on_menuList_itemSelected(self, value):
         self.label.set('value', u'The selected value was "%s"' % value)
+
+    def on_menuList2_itemSelected(self, value):
+        # Get the widget associated with the value, which in this case
+        # is a widget ID.
+        menuItem = self.id_widget[value]
+        self.label2.set('value', menuItem.counterMessage())
